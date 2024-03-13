012a56d6d4e675b8440ee592354315c9bd756d3f
Martien Dermawan Tanama <martiendt@gmail.com>
2024-03-13 08:41:04 +0700

    up staging

--- src/modules/accounting-chart-of-account/controllers/retrieve-all.controller.ts
+++ src/modules/accounting-chart-of-account/controllers/retrieve-all.controller.ts
@@ -12,13 +12,15 @@ export const retrieveAllChartOfAccountController: IController = async (controlle
     // 2. define repository
     const retrieveAllRepository = new RetrieveAllRepository(controllerInput.dbConnection)
     // 3. handle business rules
+    console.log(controllerInput.httpRequest.query)
     const query: IQuery = {
       fields: (controllerInput.httpRequest.query.fields as string) ?? '',
       filter: (controllerInput.httpRequest.query.filter as object) ?? {},
       page: Number(controllerInput.httpRequest.query.page ?? 1),
-      page_size: Number(controllerInput.httpRequest.query.pageSize ?? 10),
+      page_size: Number(controllerInput.httpRequest.query.page_size ?? 10),
       sort: (controllerInput.httpRequest.query.sort as string) ?? '',
     }
+    console.log('query json', JSON.stringify(query))
     const response = await RetrieveAllChartOfAccountUseCase.handle({ query: query }, { retrieveAllRepository })
     await session.commitTransaction()
     // 4. return response to client
--- src/modules/accounting-chart-of-account/repositories/retrieve-all.repository.ts
+++ src/modules/accounting-chart-of-account/repositories/retrieve-all.repository.ts
@@ -10,9 +10,27 @@ export class RetrieveAllRepository implements IRetrieveAllRepository {
   async handle(query: IQuery, options?: unknown): Promise<IRetrieveAllOutput> {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const pipeline: any[] = []
-    if (query && query.filter) {
-      pipeline.push({ $match: { ...query.filter } })
+    const filters = []
+    if (query?.filter?.name) {
+      filters.push({ name: { $regex: query?.filter?.name, $options: 'i' } })
     }
+    if (query?.filter?.number) {
+      filters.push({ number: { $regex: query?.filter?.number, $options: 'i' } })
+    }
+    if (query?.filter?.type) {
+      filters.push({ type: { $regex: query?.filter?.type, $options: 'i' } })
+    }
+    if (query?.filter?.subledger) {
+      filters.push({ subledger: { $regex: query?.filter?.subledger, $options: 'i' } })
+    }
+    const filter = {
+      $or: [...filters],
+    }
+    if (filters.length) {
+      pipeline.push({ $match: { ...filter } })
+    }
+    console.log(pipeline[0])
+    console.log(query)
     return await this.database.collection(this.collection).aggregate(pipeline, query, options)
   }
 }
