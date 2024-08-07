28457aabd662c5e19918a8ffb2b3407660c48701
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-07 16:22:47 +0700

    c

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,10 +1,6 @@
 import { BaseErrorHandler, type IDocument, type ISchemaValidation } from '@point-hub/papi'
 import Validatorjs from 'validatorjs'
 
-import { registerValidationPassword } from './validation-password'
-
-registerValidationPassword()
-
 // https://github.com/mikeerickson/validatorjs
 export const schemaValidation: ISchemaValidation = async (document: IDocument, schema: IDocument) => {
   const validation = new Validatorjs(document, schema)
