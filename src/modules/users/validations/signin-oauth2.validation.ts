7d63e1523c1adebf6a5fa07bb34923164726991f
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-08 01:37:45 +0700

    d

--- src/modules/users/validations/signin-oauth2.validation.ts
+++
@@ -1,4 +0,0 @@
-export const signinValidation = {
-  username: ['required', 'string'],
-  password: ['required', 'string'],
-}
--- src/modules/users/validations/verify-email.validation.ts
+++
@@ -1,3 +0,0 @@
-export const verifyEmailValidation = {
-  code: ['required', 'string'],
-}
