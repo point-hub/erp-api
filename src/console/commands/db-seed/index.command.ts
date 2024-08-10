02ffbd0df318ecf01ba440aa79f268afa37d84cb
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-10 07:23:39 +0700

    x

--- src/console/commands/db-seed/index.command.ts
+++ src/console/commands/db-seed/index.command.ts
@@ -9,28 +9,44 @@ export default class DbSeedCommand extends BaseConsoleCommand {
       name: 'db:seed',
       description: 'Seed database',
       summary: 'Seed database',
-      arguments: [],
-      options: [],
+      arguments: [
+        {
+          name: 'module',
+          description: 'Directory name of your module',
+        },
+        {
+          name: 'collection',
+          description: 'Collection name of your module',
+        },
+      ],
+      options: [
+        {
+          type: 'string',
+          flag: '--filename',
+          description: 'Filename',
+        },
+      ],
     })
   }
   async handle(): Promise<void> {
     try {
       await this.dbConnection.open()
-      await this.seed('counters', 'counters')
-      await this.seed('roles', 'roles')
+      const fileName = (this.opts['--filename'] as string) ?? 'seed'
+      await this.seed(this.args['module'], this.args['collection'], fileName)
     } catch (error) {
       console.error(error)
     } finally {
       this.dbConnection.close()
     }
   }
-  private async seed(directoryName: string, collectionName: string): Promise<ICreateManyOutput> {
+
+  private async seed(directoryName: string, collectionName: string, fileName: string): Promise<void> {
     console.info(`[seed] seeding ${collectionName} data`)
     // get seeder from module
-    const { seeds } = await import(`@/modules/${directoryName}/seed`)
+    const { seeds } = await import(`@/modules/${directoryName}/${fileName}`)
     // delete all data inside collection
     await this.dbConnection.collection(collectionName).deleteAll()
     // insert new seeder data
-    return await this.dbConnection.collection(collectionName).createMany(seeds)
+    await this.dbConnection.collection(collectionName).createMany(seeds)
   }
 }
